29c68df234253bd19ba6e11276c021060057ed6c
Daruuu <thedaruny16@gmail.com>
2025-07-03 22:36:49 +0200

    add problem of DiagonalDifference and testing

---
+++ src/main/java/com/daruuu/HackerRank/diagonaldifference03/DiagonalDifference.java
@@ -0,0 +1,54 @@
+package com.daruuu.HackerRank.diagonaldifference03;
+
+import java.util.List;
+
+public class DiagonalDifference {
+
+/*
+    Sample Input
+
+    STDIN      Function
+    -----      --------
+        3       arr[][] sizes n = 3, m = 3
+    11 2 4      arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+    4 5 6
+    10 8 -12
+
+    Sample Output:
+        15
+    Explanation::
+    The primary diagonal is:    11 + 5 + -12
+    Sum across the primary diagonal: .
+    The secondary diagonal is:  4 + 5 + 10
+*/
+
+    public static int   diagonalDifference(List<List<Integer>> arr)
+    {
+        List<Integer>   rows;
+        int sumPrimary;
+        int sumSecundary;
+        int i;
+        int j;
+        int n;
+
+        sumPrimary = 0;
+        sumSecundary = 0;
+        n = arr.size();
+        i = 0;
+        while (i < n)
+        {
+            rows = arr.get(i);
+            j = 0;
+            while (j < n)
+            {
+                if (i == j)
+                    sumPrimary += rows.get(j);  //  diagonal principal
+                if (j == n - 1 - i)
+                    sumSecundary += rows.get(j);    //diagonal secundaria
+                j++;
+            }
+            i++;
+        }
+        return (Math.abs(sumPrimary - sumSecundary));
+    }
+}
---
+++ src/test/java/com/daruuu/HackerRank/diagonaldifference03/DiagonalDifferenceTest.java
@@ -0,0 +1,31 @@
+package com.daruuu.HackerRank.diagonaldifference03;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiagonalDifferenceTest {
+
+    @Test
+    void shouldReturnValidOutput_diagonalDifference() {
+        //  given
+        List<List<Integer>> matrixTwoD = new ArrayList<>();
+        int     expectedOutput;
+
+//        matrixTwoD.add(Arrays.asList(3));
+        matrixTwoD.add(Arrays.asList(11, 2, 4));
+        matrixTwoD.add(Arrays.asList(4, 5, 6));
+        matrixTwoD.add(Arrays.asList(10, 8, -12));
+        expectedOutput = 15;
+
+        //  when
+        int diagonalDifference = DiagonalDifference.diagonalDifference(matrixTwoD);
+
+        //  then
+        assertEquals(expectedOutput, diagonalDifference);
+    }
+}

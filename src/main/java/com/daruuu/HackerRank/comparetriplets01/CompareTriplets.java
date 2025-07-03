33cc0e99d0c6b651edf8e748362fc6e281848565
Daruuu <thedaruny16@gmail.com>
2025-07-03 18:04:02 +0200

    new directory add HackerRank | exer01 CompareTriplets

---
+++ src/main/java/com/daruuu/HackerRank/comparetriplets01/CompareTriplets.java
@@ -0,0 +1,30 @@
+package com.daruuu.HackerRank.comparetriplets01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompareTriplets {
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer>b)
+    {
+        List<Integer>   result = new ArrayList<>();
+        int i;
+        int left;
+        int right;
+
+        i = 0;
+        left = 0;
+        right = 0;
+        while (i < a.size())
+        {
+            if (a.get(i) < b.get(i))
+                left ++;
+            else if (a.get(i) > b.get(i))
+                right ++;
+            i++;
+        }
+        result.add(right);
+        result.add(left);
+        return (result);
+    }
+}

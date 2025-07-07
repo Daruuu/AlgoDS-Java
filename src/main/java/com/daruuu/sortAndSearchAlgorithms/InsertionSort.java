c1a3e380a8b8e0a47da6c7e4805e659b68a696f0
Daruuu <thedaruny16@gmail.com>
2025-07-07 18:05:52 +0200

    add InsertionSort and edge cases in testing

---
+++ src/main/java/com/daruuu/sortAndSearchAlgorithms/InsertionSort.java
@@ -0,0 +1,71 @@
+package com.daruuu.sortAndSearchAlgorithms;
+
+import java.util.List;
+
+public class InsertionSort {
+    /*
+    ## 📘 Problem #5 — Insertion Sort
+
+    **🧠 Title:**
+    Sort an Array Using Insertion Sort
+
+    **📋 Description:**
+    Implement the **Insertion Sort** algorithm to sort an array of integers in **ascending order**.
+    Insertion Sort builds the sorted array one element at a time by taking each element
+    and placing it in its correct position among the previously sorted elements.
+
+    You must sort the array **in place**, without using built-in sort methods.
+
+    **📥 Input Example:**
+    ```java
+    nums = [8, 4, 2, 9, 1]
+    ```
+    **📤 Output Example:**
+
+    ```java
+    [1, 2, 4, 8, 9]
+    ```
+    ---
+    ### ✅ Constraints:
+
+    * No usage of `.sort()` or any library sorting utilities.
+    * Must use the **Insertion Sort** technique (not Bubble or Selection).
+    * Time complexity:
+
+      * Worst case: **O(n²)**
+      * Best case (already sorted): **O(n)**
+    * Space complexity: **O(1)**
+
+    ---
+
+    ### 💡 Hint (if needed):
+
+    In `Insertion Sort`, you iterate from index `1` to `n-1`, and for each element, you compare it with the previous sorted elements (to the left) and **shift them** if they’re larger, then insert the current value into its correct position.
+    ---
+    Cuando termines tu implementación, la revisamos con ojos de entrevista: legibilidad, eficiencia y explicación técnica clara. ¿Listo?
+     */
+//    public static List<Integer> sortArrayUsingInsertionSort(List<Integer> numList)
+    public static int[] sortArrayUsingInsertionSort(int[] numList)
+    {
+        int i;
+        int j;
+        int key;
+
+        if (numList == null || numList.length == 0)
+            return (null);
+        i = 1;
+        while (i < numList.length)
+        {
+            key = numList[i];
+            j = i - 1;
+            while (j >= 0 && numList[j] > key)
+            {
+                numList[j + 1] = numList[j];
+                j--;
+            }
+            numList[j + 1] = key;
+            i++;
+        }
+        return (numList);
+    }
+}
---
+++ src/test/java/com/daruuu/sortAndSearchAlgorithms/InsertionSortTest.java
@@ -0,0 +1,125 @@
+package com.daruuu.sortAndSearchAlgorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InsertionSortTest {
+
+    @Test
+    void    shouldReturnSortedArray_whenArrayExists() {
+        //  given
+        int []  inputArray = {8, 4, 2, 9, 1};
+        int []  expectedOutput = {1, 2, 4, 8, 9};
+
+        //  when
+        int []  result = InsertionSort.sortArrayUsingInsertionSort(inputArray);
+
+        //  then
+        assertArrayEquals(expectedOutput, result);
+    }
+
+    @Test
+    void    shouldReturnNullArray_whenArrayIsNull() {
+        //  given
+        int []  inputArray = null;//{8, 4, 2, 9, 1};
+        int []  expectedOutput = null;//{1, 2, 4, 8, 9};
+
+        //  when
+        int []  result = InsertionSort.sortArrayUsingInsertionSort(inputArray);
+
+        //  then
+        assertArrayEquals(expectedOutput, result);
+    }
+
+    @Test
+    void    shouldReturnArraySorted_whenArrayHasDuplicateValue() {
+        //  given
+        int []  inputArray = {8, 4, 2, 9, 1, 4};
+        int []  expectedOutput = {1, 2, 4, 4, 8, 9};
+
+        //  when
+        int []  result = InsertionSort.sortArrayUsingInsertionSort(inputArray);
+
+        //  then
+        assertArrayEquals(expectedOutput, result);
+    }
+
+    @Test
+    void    shouldReturnOneValue_whenArrayHasOnlyOneValue() {
+        //  given
+        int []  inputArray = {8};
+        int []  expectedOutput = {8};
+
+        //  when
+        int []  result = InsertionSort.sortArrayUsingInsertionSort(inputArray);
+
+        //  then
+        assertArrayEquals(expectedOutput, result);
+    }
+
+    @Test
+    void    shouldReturnNull_whenArrayIsEmpty() {
+        //  given
+        int []  inputArray = {};
+        int []  expectedOutput = null;
+
+        //  when
+        int []  result = InsertionSort.sortArrayUsingInsertionSort(inputArray);
+
+        //  then
+        assertArrayEquals(expectedOutput, result);
+    }
+
+    @Test
+    void    shouldReturnSortArray_whenArrayIsSortInvert() {
+        //  given
+        int []  inputArray = {7, 5, 4, 3, 2, 1, 0};
+        int []  expectedOutput = {0, 1, 2, 3, 4, 5, 7};
+
+        //  when
+        int []  result = InsertionSort.sortArrayUsingInsertionSort(inputArray);
+
+        //  then
+        assertArrayEquals(expectedOutput, result);
+    }
+
+    @Test
+    void    shouldReturnSortArray_whenHasPositiveAndNegativesNumbers() {
+        //  given
+        int []  inputArray = {7, -5, 4, 30, -2, -110};
+        int []  expectedOutput = {-110, -5, -2, 4, 7, 30};
+
+        //  when
+        int []  result = InsertionSort.sortArrayUsingInsertionSort(inputArray);
+
+        //  then
+        assertArrayEquals(expectedOutput, result);
+    }
+
+    @Test
+    void    shouldReturnSameArray_whenAllNumsInArrayAreEqual() {
+        //  given
+        int []  inputArray = {5, 5, 5, 5, 5, 5};
+        int []  expectedOutput = {5, 5, 5, 5, 5, 5};
+
+        //  when
+        int []  result = InsertionSort.sortArrayUsingInsertionSort(inputArray);
+
+        //  then
+        assertArrayEquals(expectedOutput, result);
+    }
+
+    @Test
+    void    shouldReturnSameArray_whenArrayAreSort() {
+        //  given
+        int []  inputArray = {-100, 1, 2, 3, 4, 5};
+        int []  expectedOutput = {-100, 1, 2, 3, 4, 5};
+
+        //  when
+        int []  result = InsertionSort.sortArrayUsingInsertionSort(inputArray);
+
+        //  then
+        assertArrayEquals(expectedOutput, result);
+    }
+}

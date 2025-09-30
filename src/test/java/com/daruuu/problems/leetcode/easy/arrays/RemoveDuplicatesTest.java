45cbdd5c7d40ade02e8424e24609464e4aaceb5a
Daruuu <thedaruny16@gmail.com>
2025-09-30 12:46:28 +0200

    add new problem RemoveDuplicate.java

---
+++ src/test/java/com/daruuu/problems/leetcode/easy/arrays/RemoveDuplicatesTest.java
@@ -0,0 +1,19 @@
+package com.daruuu.problems.leetcode.easy.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveDuplicatesTest {
+
+    @Test
+    void should_removeDuplicates_whenTwoNumberEquals() {
+        //  Arrange
+
+        //  Act
+
+        //  Assert
+
+    }
+
+}

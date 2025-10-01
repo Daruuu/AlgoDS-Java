0bb80f449a489ec95400a912910037398900d008
Daruuu <thedaruny16@gmail.com>
2025-10-01 08:55:38 +0200

    add new case test for CheckIfNandItsDoubleExist

--- src/test/java/com/daruuu/problems/leetcode/easy/arrays/CheckIfNandItsDoubleExistTest.java
+++ src/test/java/com/daruuu/problems/leetcode/easy/arrays/CheckIfNandItsDoubleExistTest.java
@@ -7,18 +7,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class CheckIfNandItsDoubleExistTest {
 
     @Test
+    void should_returnFalse_when_checkIfFirstNumberIsEqualofMultiplyOfSecondAndThirdNumberInArray() {
+        //  arrange
+
+        int []arr = {3,1,7,11};
+
+        //  act
+        boolean result = CheckIfNandItsDoubleExist.checkIfExist(arr);
+
+        //  assert
+        assertFalse(result);
+
+    }
+
+    @Test
     void should_returnTrue_when_checkIfFirstNumberIsEqualofMultiplyOfSecondAndThirdNumberInArray() {
         //  arrange
 
-//        int []arr = {3,1,7,11};
         int []arr = {10,2,5,11};
 
         //  act
         boolean result = CheckIfNandItsDoubleExist.checkIfExist(arr);
 
         //  assert
-//        assertFalse(result);
         assertTrue(result);
-
     }
 }
